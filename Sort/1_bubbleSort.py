7e22cee292a13a0fc31f2f757db687f422e48c44
WarachotInkun <warachot.iq@gmail.com>
2024-10-09 19:21:44 +0700

    Update 1_bubbleSort.py

--- Sort/1_bubbleSort.py
+++ Sort/1_bubbleSort.py
@@ -6,24 +6,12 @@ def bubble(lst):
             if lst[i] > lst[i+1]  :
                 temp = lst[i]
                 swap = True
-                lst[i] ,lst[i+1]  = lst[i+1],lst[i] 
-                
-
+                lst[i] ,lst[i+1]  = lst[i+1],lst[i]                 
         if not swap or len(lst)-n-1  ==1:
             print(f"last step : {lst} move[{temp}]")
             return
         else:
             print(f"{n+1} step : {lst} move[{temp}]")
-
-
-
-
-
-
-
-
-
-
 
 
 inp = list(map(int,input("Enter Input : ").split(" ")))
